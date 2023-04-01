e8f9b4b1df1bbaceafca28ed5b45eb983bd58125
SerhiiOd <115508914+SerhiiOd@users.noreply.github.com>
2023-04-01 09:01:25 +0000

    Deploying to gh-pages from @ SerhiiOd/goit-js-hw-08@c1aae47bf49de5977aa171f4841f8aee2506e14d 🚀

--- 03-feedback.907772f6.js
+++
@@ -1,2 +0,0 @@
-const e={form:document.querySelector(".feedback-form"),textarea:document.querySelector(".feedback- form textarea")};e.form.addEventListener("submit",(function(e){})),e.textarea.addEventListener("input",(function(e){const t=e.currentTurget.value;localStorage.setItem("feedback-msg",t)}));
-//# sourceMappingURL=03-feedback.907772f6.js.map
--- 03-feedback.a1e76ddc.js
+++
@@ -1,2 +0,0 @@
-!function(){var e={form:document.querySelector(".feedback-form"),textarea:document.querySelector(".feedback- form textarea")};e.form.addEventListener("submit",(function(e){})),e.textarea.addEventListener("input",(function(e){var t=e.currentTurget.value;localStorage.setItem("feedback-msg",t)}))}();
-//# sourceMappingURL=03-feedback.a1e76ddc.js.map
